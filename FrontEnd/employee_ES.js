32fe9b5cd016f4f5ef4adcdd6e3cd628eb047e62
Nahom Abebe <58399654+nahom-hub@users.noreply.github.com>
2021-07-08 17:05:00 -0500

    pull the change mad

---
+++ FrontEnd/employee_ES.js
@@ -0,0 +1,80 @@
+const url = 'http://localhost:8084/P1Demo/' //I grabbed this base URL from postman
+//it's also what shows up when you run as server and get your 404 page
+//Eventually, we'll use this base URL and make calls to the server by adding endpoints to it
+
+//add functionality to our button using an event listener
+document.getElementById('getAvengerButton').addEventListener('click', assembleFunc);
+//so when this button gets clicked, the function called assembleFunc will run
+//we could have used: document.getElementById('getAvengerButton').onClick(assembleFunc);
+
+//this function will get all avengers from our Java server
+async function assembleFunc() { //async returns a promise (which fetch returns)
+
+    //we will send a fetch request to get our avenger data
+    //await wmakes the async functions wait until the promise returns with the fetched data
+    let response = await fetch(url + 'avengers');
+
+    if(response.status === 200) { //if the request is successful...
+        console.log(response); //just to see what comes back for debug
+
+        let data = await response.json(); //get the JSON data from the response, turn it into JS object
+
+
+        //now, I want to put each avenger into my table
+        for(let avenger of data) { //for every avenger in the data variable/object
+
+            console.log(avenger); //just for debug, print the avenger in the console
+
+            let row = document.createElement("tr"); //create a table row
+
+            let cell = document.createElement("td"); //create a cell for the field
+            cell.innerHTML = avenger.av_id; //fill the cell with avenger data
+            row.appendChild(cell) //this row now has the first cell (av_id)
+
+            //the we'll do this^ for each field in the avenger model
+            let cell2 = document.createElement("td");
+            cell2.innerHTML = avenger.av_name;
+            row.appendChild(cell2);
+
+            let cell3 = document.createElement("td");
+            cell3.innerHTML = avenger.av_power;
+            row.appendChild(cell3);
+
+            let cell4 = document.createElement("td");
+            cell4.innerHTML = avenger.first_name;
+            row.appendChild(cell4);
+
+            let cell5 = document.createElement("td");
+            cell5.innerHTML = avenger.last_name;
+            row.appendChild(cell5);
+
+            let cell6 = document.createElement("td");
+            cell6.innerHTML = avenger.power_level;
+            row.appendChild(cell6);
+
+            //if the avenger has a home, just fill the cell with the home name
+            if(avenger.home_fk != null){
+                console.log("home came through")
+                let cell7 = document.createElement("td");
+                cell7.innerHTML = avenger.home_fk.homeName;
+                row.appendChild(cell7);
+            } else { //otherwise, still append the cell but leave it empty
+                let cell7 = document.createElement("td");
+                row.appendChild(cell7);
+            }
+
+
+
+            document.getElementById("avengerBody").appendChild(row);
+            //so the variable "row" we created alllll the way at the top of the for loop 
+            //will be appended to our empty table body in the HTML
+
+        }
+
+    }
+
+}
+
+
+
+
---
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -0,0 +1,40 @@
+package com.revature.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	public static void main(String[] args) {
+		
+		//here we're just testing whether our connection (from the ConnectionUtil Class) is successful
+		//remember - the getConnection() method will return a Connection object if you successfully reach the database
+		try(Connection conn = ConnectionUtil.getConnection()) {
+			System.out.println("connection successful");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+}
+	
+	
+public static Connection getConnection() throws SQLException {
+		
+		
+		try {
+			Class.forName("org.postgresql.Driver"); //try to find and return the postgresql Driver Class
+		} catch (ClassNotFoundException e) {
+			System.out.println("Class wasn't found :(");
+			e.printStackTrace(); //prints the exception message to the console
+		}
+		
+		
+		String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=project_one";
+		String username = "postgres";
+		String password = "password"; 
+		
+		
+		return DriverManager.getConnection(url, username, password);
+	
+}
+}
